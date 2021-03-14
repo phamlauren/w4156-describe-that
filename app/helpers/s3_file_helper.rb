bf670d97209b4d4133e4dc753e8820cd3c02e637
Vishnu Nair <dev@nairvish.com>
2021-03-14 18:44:05 -0400

    Implementing S3 file upload and description file unique name generator.

---
+++ app/helpers/s3_file_helper.rb
@@ -0,0 +1,18 @@
+require 'aws-sdk'
+require 'securerandom'
+
+module S3FileHelper
+  def upload_file(desired_name, file_io_contents)
+    s3 = Aws::S3::Client.new
+    obj = s3.bucket(ENV["S3_BUCKET_NAME"]).object(desired_name)
+    obj.put(body: file_io_contents)  # file_io_contents IS FROM params[:file].to_io
+    true
+  rescue StandardError => e
+    # TODO: Log error?
+    false
+  end
+
+  def get_presigned_dl_url_for_file(file_loc)
+
+  end
+end
