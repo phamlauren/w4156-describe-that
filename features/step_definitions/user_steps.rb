b2c4b32c77f082c84cfa50f3805d8658a31d1a17
Lauren Pham <lauren.pham@focus.org>
2021-03-14 13:22:23 -0400

    add steps specifically for email and password fields

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,7 +1,7 @@
 When /^I fill in the "email" field with "([^"]*)"$/ do |value|
-    fill_in('email', :with => value)
+    fill_in('user_email', :with => value)
 end
 
 When /^I fill in the "password" field with "([^"]*)"$/ do |value|
-    fill_in('password', :with => value)
+    fill_in('user_password', :with => value)
 end
