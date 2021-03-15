cd497c318436c188937fb9f51a4e176777dfced5
Vishnu Nair <dev@nairvish.com>
2021-03-14 21:02:33 -0400

    Finishing function get presigned download URL for file in S3 bucket.

--- app/helpers/s3_file_helper.rb
+++ app/helpers/s3_file_helper.rb
@@ -23,7 +23,16 @@ module S3FileHelper
     false
   end
 
-  def get_presigned_dl_url_for_file(file_loc)
+  def self.get_presigned_dl_url_for_file(file_name, validity_sec = 300)
+    s3 = Aws::S3::Client.new
+    signer = Aws::S3::Presigner.new(client: s3)
 
+    signer.presigned_url(
+      :get_object,
+      bucket: ENV['S3_BUCKET_NAME'],
+      key: file_name,
+      use_accelerate_endpoint: true,
+      expires_in: validity_sec
+    )
   end
 end
