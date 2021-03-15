483b432026d542149534b4eb03089cb10cd0868d
Lauren Pham <lauren.pham@focus.org>
2021-03-15 08:37:43 -0400

    comment out for merge into main

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,7 +1,8 @@
 require 'securerandom'
 
 class Description < ApplicationRecord
-  belongs_to :description_track, optional: true # optional: true is temporary for cucumber
+  belongs_to :description_track, # optional: true # optional: true is temporary for cucumber
+                                                # remove for deploy and prod
   belongs_to :voice, optional: true
   enum desc_type: { recorded: "recorded", generated: "generated" }
 
