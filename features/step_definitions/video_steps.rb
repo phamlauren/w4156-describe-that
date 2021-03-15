a60ace3cb1d7f6739f89a4f3bc88e8cc577c727c
Lauren Pham <lauren.pham@focus.org>
2021-03-15 01:58:11 -0400

    create! catches err

--- features/step_definitions/video_steps.rb
+++ features/step_definitions/video_steps.rb
@@ -1,17 +1,17 @@
 Given /the following YouTube videos exist/ do |videos_table|
     videos_table.hashes.each do |video|
-      Video.create video
+      Video.create! video
     end
 end
 
 Given /the following description tracks exist/ do |description_tracks_table|
     description_tracks_table.hashes.each do |description_track|
-      DescriptionTrack.create description_track
+      DescriptionTrack.create! description_track
     end
 end
 
 Given /the following descriptions exist/ do |descriptions_table|
     descriptions_table.hashes.each do |description|
-      Description.create description
+      Description.create! description
     end
 end
