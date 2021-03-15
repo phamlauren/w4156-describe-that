d343f4e5cd9068718a3d8a7bd56a2831e02c8abe
SheronW <sheronw1174@gmail.com>
2021-03-14 21:39:57 -0400

    block invalid route for new describe video

--- app/controllers/description_tracks_controller.rb
+++ app/controllers/description_tracks_controller.rb
@@ -1,5 +1,10 @@
 class DescriptionTracksController < ActionController::Base
     def new
+        match = /^(?:https:\/\/)?(?:www.youtube.com\/watch\?v=|youtu.be\/)([^?]+)(?:\?.*)?$/.match(params[:yt_url])
+        vid = match.captures[0]
+        redirect_to("welcome#index")
+    rescue NoMethodError
+        # should not have a route without a YouTube URL
         redirect_to("welcome#index")
     end
 end
