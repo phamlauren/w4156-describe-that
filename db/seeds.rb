fe116e5c3420665cfd15c3875c8f56fd80df3308
Vishnu Nair <dev@nairvish.com>
2021-03-14 23:59:15 -0400

    Adding voice seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# All voices.
+
+# US voices.
+Voice.create(common_name: "Voice US A", system_name: "en-US-Wavenet-A", provider: "google_tts")
+Voice.create(common_name: "Voice US B", system_name: "en-US-Wavenet-B", provider: "google_tts")
+Voice.create(common_name: "Voice US C", system_name: "en-US-Wavenet-C", provider: "google_tts")
+Voice.create(common_name: "Voice US D", system_name: "en-US-Wavenet-D", provider: "google_tts")
+Voice.create(common_name: "Voice US E", system_name: "en-US-Wavenet-E", provider: "google_tts")
+Voice.create(common_name: "Voice US F", system_name: "en-US-Wavenet-F", provider: "google_tts")
+Voice.create(common_name: "Voice US G", system_name: "en-US-Wavenet-G", provider: "google_tts")
+Voice.create(common_name: "Voice US H", system_name: "en-US-Wavenet-H", provider: "google_tts")
+Voice.create(common_name: "Voice US I", system_name: "en-US-Wavenet-I", provider: "google_tts")
+Voice.create(common_name: "Voice US J", system_name: "en-US-Wavenet-J", provider: "google_tts")
+
+# GB voices.
+Voice.create(common_name: "Voice GB A", system_name: "en-GB-Wavenet-A", provider: "google_tts")
+Voice.create(common_name: "Voice GB B", system_name: "en-GB-Wavenet-B", provider: "google_tts")
+Voice.create(common_name: "Voice GB C", system_name: "en-GB-Wavenet-C", provider: "google_tts")
+Voice.create(common_name: "Voice GB D", system_name: "en-GB-Wavenet-D", provider: "google_tts")
+Voice.create(common_name: "Voice GB E", system_name: "en-GB-Wavenet-E", provider: "google_tts")
+Voice.create(common_name: "Voice GB F", system_name: "en-GB-Wavenet-F", provider: "google_tts")
