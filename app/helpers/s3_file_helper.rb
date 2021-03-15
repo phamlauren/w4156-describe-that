6dff3f64de62df3f212fb30d2e77f09dd81b6690
Vishnu Nair <dev@nairvish.com>
2021-03-14 21:01:31 -0400

    Fixing file upload function.

--- app/helpers/s3_file_helper.rb
+++ app/helpers/s3_file_helper.rb
@@ -1,11 +1,23 @@
 require 'aws-sdk-s3'
 
 module S3FileHelper
-  def upload_file(desired_name, file_io_contents)
+  def self.upload_file(desired_name, file_io_contents, metadata = nil)
     s3 = Aws::S3::Client.new
     obj = s3.bucket(ENV["S3_BUCKET_NAME"]).object(desired_name)
-    obj.put(body: file_io_contents)  # file_io_contents IS FROM params[:file].to_io
-    true
+
+    # file_io_contents IS FROM params[:file].to_io. Can be String, StringIO, or File.
+    if metadata.nil?
+      response = obj.put(body: file_io_contents)
+    else
+      response = obj.put(body: file_io_contents, metadata: metadata)
+    end
+
+    # check if successfully uploaded
+    if response.etag
+      true
+    else
+      false
+    end
   rescue StandardError => e
     # TODO: Log error?
     false
