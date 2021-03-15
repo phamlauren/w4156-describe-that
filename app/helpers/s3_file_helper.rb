73a8e580517ee98ae124358d11b98b3a5f8e1334
Vishnu Nair <dev@nairvish.com>
2021-03-14 21:32:07 -0400

    Fixing all bugs.

--- app/helpers/s3_file_helper.rb
+++ app/helpers/s3_file_helper.rb
@@ -2,8 +2,10 @@ require 'aws-sdk-s3'
 
 module S3FileHelper
   def self.upload_file(desired_name, file_io_contents, metadata = nil)
-    s3 = Aws::S3::Client.new
-    obj = s3.bucket(ENV["S3_BUCKET_NAME"]).object(desired_name)
+    client = Aws::S3::Client.new
+    resource = Aws::S3::Resource.new(client: client)
+    bucket = resource.bucket(ENV["S3_BUCKET_NAME"])
+    obj = bucket.object(desired_name)
 
     # file_io_contents IS FROM params[:file].to_io. Can be String, StringIO, or File.
     if metadata.nil?
@@ -20,6 +22,7 @@ module S3FileHelper
     end
   rescue StandardError => e
     # TODO: Log error?
+    puts e.inspect
     false
   end
 
@@ -31,8 +34,7 @@ module S3FileHelper
       :get_object,
       bucket: ENV['S3_BUCKET_NAME'],
       key: file_name,
-      use_accelerate_endpoint: true,
-      expires_in: validity_sec
+      expires_in: validity_sec  # can be up to one week
     )
   end
 end
