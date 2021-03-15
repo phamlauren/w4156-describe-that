4b113e4785b40696288f17a9d42a3d053b131b25
Lauren Pham <lauren.pham@focus.org>
2021-03-15 19:33:29 -0400

    remove comments, unused code

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -40,25 +40,6 @@ class VideoController < ApplicationController
       @yt_info = video_info @video.yt_video_id
       render "request_video" if @descriptions.empty?
     end
-    # renders app/view/video/show.html.erb by default
-  end
-
-  # Create a description
-  def add_description
-    # pass yt_video_id from show method
-    # @video = Video.find(yt_video_id)
-    @video = Video.find(params[:id])
-    @description_track = DescriptionTrack.find_by('video_id': @video.id)
-    # TO DO ------->
-    # pass params to description model method to create descriptions
-    #     with description.desc_track_id = @description_track.id
-    # the generate tts method belonging to the description model
-    #     may then be called from within the create description
-    #     model method itself before returning here
-    # <-------------
-    # Get updated list of descriptions, including newly created one
-    @descriptions = Description.where(desc_track_id: @description_track.id)
-    # TO DO: then redirect to show path for this video
   end
 
   def request_video
