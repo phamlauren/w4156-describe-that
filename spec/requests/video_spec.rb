3dbf308c5920742d56830dd9de3362375dc480c5
Lauren Pham <lauren.pham@focus.org>
2021-03-15 22:03:47 -0400

    need controller spec to increase code coverage

---
+++ spec/requests/video_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Videos", type: :request do
+  describe "GET /" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "GET /video" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "GET /video/:id" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "GET /video/:id/describe" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "POST /video/:id/describe" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "POST /video/:id/request" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "GET /user" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "GET /user/new" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "POST /user" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "GET /user/login" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+  describe "POST /user/login" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
