daea3cd5e2b0f60ffa5ee7f368b1acf8829f3310
Lauren Pham <lauren.pham@focus.org>
2021-03-15 21:58:09 -0400

    creates descriptions for description tracks

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -42,13 +42,32 @@ RSpec.describe Video, type: :model do
     videos_table.each do |video|
       Video.create! video
     end
-    expect(Video.exists?(:yt_video_id=>200)).to eq(true)
+
+    # Description tracks must exist for description to exist
+    desc_track_table = [
+      {
+        video_id: 1,
+        track_author_id: 1,
+        lang: 'en',
+        is_generated: 1
+      },
+      {
+        video_id: 2,
+        track_author_id: 2,
+        lang: 'en',
+        is_generated: true
+      },
+    ]
+    desc_track_table.each do |desc_track|
+      DescriptionTrack.create! desc_track
+    end
+
   end
 
   # For each seeded video, create a description track
-  describe "model" do
+  describe "video model" do
     # Seed videos: simulates retrieving them from YouTube API
-    it "successfully creates videos" do
+    it "creates videos" do
       videos_table = [
         {
           yt_video_id: 500
@@ -69,20 +88,8 @@ RSpec.describe Video, type: :model do
       expect(Video.exists?(:yt_video_id=>600)).to eq(true)
     end
 
-    it "successfully creates description tracks for videos" do
+    it "creates description tracks for videos" do
       desc_track_table = [
-        {
-          video_id: 1,
-          track_author_id: 1,
-          lang: 'en',
-          is_generated: 1
-        },
-        {
-          video_id: 2,
-          track_author_id: 2,
-          lang: 'en',
-          is_generated: true
-        },
         {
           video_id: 3,
           track_author_id: 3,
@@ -99,38 +106,58 @@ RSpec.describe Video, type: :model do
       desc_track_table.each do |desc_track|
         DescriptionTrack.create! desc_track
       end
-      expect(DescriptionTrack.exists?(:video_id=>1, :track_author_id=>1)).to eq(true)
+      expect(DescriptionTrack.exists?(:video_id=>4, :track_author_id=>1)).to eq(true)
+    end
+
+    # For each description track, create a description
+    it "creates descriptions for description tracks" do
+      desc_table = [
+        {
+          desc_track_id: 1,
+          start_time_sec: '00:00:10',
+          pause_at_start_time: 'en',
+          audio_file_loc: 'loc1',
+          desc_text: 'Description text for desc_track_id 1',
+        },
+        {
+          desc_track_id: 2,
+          start_time_sec: '00:10:00',
+          pause_at_start_time: 'en',
+          audio_file_loc: 'loc2',
+          desc_text: 'Description text for desc_track_id 2',
+        },
+      ]
+      desc_table.each do |desc|
+        Description.create! desc
+      end
+      expect(Description.exists?(:desc_track_id=>1, :audio_file_loc=>'loc1')).to eq(true)
     end
 
   end
 
-  # For each description track, create a description
-  describe "create descriptions for description tracks" do
-    
-  end
 
   # For a video, get_all_desc_tracks
-  describe "get all description tracks for videos" do
+  describe "gets all description tracks for videos" do
     
   end
 
   # For a video, get_all_desc_track_with_lang
-  describe "get all description tracks with their language for videos" do
+  describe "gets all description tracks with their language for videos" do
     
   end
 
   # For a video, video_exists
-  describe "check if video exists" do
+  describe "checks if video exists" do
     
   end
 
   # For a description track, get_all_descriptions
-  describe "get all descriptions for description tracks" do
+  describe "gets all descriptions for description tracks" do
     
   end
 
   # For a description, assert generate_unique_name works
-  describe "check generation of description track audio file names" do
+  describe "checks generation of description track audio file names" do
     
   end
 
