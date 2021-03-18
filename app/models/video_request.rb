53dc4b0c8b378e2d935551c4706c29cbc0c64fbc
Vishnu Nair <dev@nairvish.com>
2021-03-17 21:43:48 -0400

    One small change.

--- app/models/video_request.rb
+++ app/models/video_request.rb
@@ -1,5 +1,5 @@
 class VideoRequest < ApplicationRecord
   belongs_to :video
   belongs_to :requester, class_name: 'User'
-  has_many :upvotes, class_name: 'VideoRequestUpvote', optional: true
+  has_many :upvotes, class_name: 'VideoRequestUpvote', foreign_key: 'video_request_id', optional: true
 end
