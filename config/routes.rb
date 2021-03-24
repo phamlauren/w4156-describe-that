4b0afc3c7995d4514053b213aa6b5ccaa10ad061
Lauren Pham <lauren.pham@focus.org>
2021-03-24 12:07:30 -0400

    add route for video requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'video#index'
 
+  # Video
   get '/video/recent', to: 'video#recently_accessed'
   get '/video', to: 'video#fetch_from_api'
   get '/video/:id', to: 'video#show'
   get 'video/:id/describe', to: 'video#describe'
   post 'video/:id/describe', to: 'video#describe'
   post '/video/:id/request', to: 'video#request_video'
+
+  # Video request
+  get '/video_requests', to: 'video_request#index'
+
+  # User
   get '/user', to: 'user#index'
   get '/user/new', to: 'user#new'
   post '/user', to: 'user#create'
