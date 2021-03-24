f190b71e0faf3bba4499445acadc847a11a7b7d9
Lauren Pham <lauren.pham@focus.org>
2021-03-24 12:14:58 -0400

    /video_requests returns list of video requests hypothetically in order of most votes to least (no seeded data to verify correct code yet)

---
+++ app/controllers/video_request_controller.rb
@@ -0,0 +1,19 @@
+class VideoRequestController < ApplicationController
+
+  def index
+    @requests_info = []
+    video_requests = VideoRequest.all.sort { |a, b| a.number_of_votes <=> b.number_of_votes }
+    puts video_requests
+    video_requests.each do |video_request|
+        video = Video.find(video_request.video_id)
+        puts video
+        video_info = video.video_info
+        # show video if we successfully get video info from youtube API
+        if !video_info.empty?
+        video_info["id"] = video.id
+        @requests_info.push(video_info)
+        end
+    end
+  end
+
+end
