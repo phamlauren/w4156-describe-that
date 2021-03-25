ffe0e716998ba6aa618004c9f927bc102b6c1c6b
SheronW <sheronw1174@gmail.com>
2021-03-25 19:13:55 -0400

    add inline/extended selection

--- app/controllers/description_controller.rb
+++ app/controllers/description_controller.rb
@@ -6,8 +6,7 @@ class DescriptionController < ApplicationController
     #@description = Description.create_with(pause_at_start_time: false, desc_type: 'generated', audio_file_loc: nil, desc_text: params[:description], voice_id: 2, voice_speed: 1.0).find_or_create_by!(desc_track_id: params[:track_id].to_i, start_time_sec: params[:time])
     #@description.desc_text = params[:description]
     #Description.where(desc_track_id: params[:track_id].to_i, start_time_sec: params[:time]).destroy_all
-
-    @description = Description.create(pause_at_start_time: false, desc_type: 'generated', audio_file_loc: "", desc_text: params[:description], voice_id: 2, voice_speed: 1.0, desc_track_id: params[:track_id].to_i, start_time_sec: params[:time])
+    @description = Description.create(pause_at_start_time: params[:pause_at_start_time]=="1" ? true : false, desc_type: 'generated', audio_file_loc: "", desc_text: params[:description], voice_id: 2, voice_speed: params[:voice_speed].to_f, desc_track_id: params[:track_id].to_i, start_time_sec: params[:time])
     render :plain => @description.generate_tts ? @description.get_download_url_for_audio_file : ""
   end
 end
