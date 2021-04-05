691b19dc371be5fff89282a28ed4fda23bfcc4fd
Lauren Pham <lauren.pham@focus.org>
2021-04-05 18:35:16 -0400

    show favorites and upvotes on dashboard

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,9 +8,6 @@ class UserController < ApplicationController
     # Test with background seeded user having sub = auth0_id = abcde
     session[:userinfo] = {"sub"=>"fdsaasdf"} if Rails.env.test?
     @user = User.find_by(auth0_id: session[:userinfo]['sub'])
-    @requests = VideoRequest.where(requester_id: @user.id)
-    @upvotes = VideoRequestUpvote.where(upvoter_id: @user.id)
-    @comments = DescriptionTrackComment.where(comment_author_id: @user.id)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :authored_description_tracks, class_name: "DescriptionTrack", foreign_key: 'track_author_id'
   has_many :comments_on_tracks, class_name: 'DescriptionTrackComment', foreign_key: 'comment_author_id'
+  has_many :video_requests, class_name: 'VideoRequest', foreign_key: 'requester_id'
+  has_many :video_request_upvotes, class_name: 'VideoRequestUpvote', foreign_key: 'upvoter_id'
+  has_many :votes_on_tracks, class_name: 'DescriptionTrackVote', foreign_key: 'voter_id'
+  has_many :video_favorites, class_name: "VideoFavorite", foreign_key: 'user_id'
 end
