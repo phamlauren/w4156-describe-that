62282b9a9a4a03828a5d22e26cf1c33c01245f39
Lauren Pham <lauren.pham@focus.org>
2021-04-09 20:42:07 -0400

    specify name of fk

--- app/models/description_track_vote.rb
+++ app/models/description_track_vote.rb
@@ -1,6 +1,6 @@
 # :nocov:
 class DescriptionTrackVote < ApplicationRecord
-  belongs_to :description_track
+  belongs_to :description_track, foreign_key: 'desc_track_id'
   belongs_to :voter, class_name: 'User'
   enum vote_dir: { up: "up", down: "down" }
 end
