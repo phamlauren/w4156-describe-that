08347ca309f3324d38697f5d4c1aaf9ed5747a9c
Lauren Pham <lauren.pham@focus.org>
2021-04-21 18:59:33 -0400

    correct form for Voice matching Vishnu's new implementation

--- spec/requests/description_spec.rb
+++ spec/requests/description_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DescriptionController, type: :controller do
 
   before (:all) do
     @author = User.find_or_create_by!(auth0_id: "fdsaasdf")
-    @voice = Voice.find_or_create_by!(common_name: "Voice US A", system_name: "en-US-Wavenet-A", provider: "google_tts")
+    @voice = Voice.find_or_create_by!(common_name: "en-US-Wavenet-A (male, Google TTS)", system_name: "en-US-Wavenet-A", language_code: 'en', country_code: 'us', gender: 'male', provider: "google_tts", natural_sample_rate_hz: '24000')
     @video = Video.find_or_create_by!(yt_video_id: "S1x76DoACB8") # "Frozen" teaser trailer
     @track = DescriptionTrack.find_or_create_by!(video_id: @video.id, track_author_id: @author.id, published: true, lang: "en")
   end
@@ -18,7 +18,7 @@ RSpec.describe DescriptionController, type: :controller do
 
   describe "POST /description/edit_generated" do
     @author = User.find_or_create_by!(auth0_id: "fdsaasdf")
-    @voice = Voice.find_or_create_by!(common_name: "Voice US A", system_name: "en-US-Wavenet-A", provider: "google_tts")
+    @voice = Voice.find_or_create_by!(common_name: "en-US-Wavenet-A (male, Google TTS)", system_name: "en-US-Wavenet-A", language_code: 'en', country_code: 'us', gender: 'male', provider: "google_tts", natural_sample_rate_hz: '24000')
     @video = Video.find_or_create_by!(yt_video_id: "S1x76DoACB8") # "Frozen" teaser trailer
     @track = DescriptionTrack.find_or_create_by!(video_id: @video.id, track_author_id: @author.id, published: true, lang: "en")
     d = Description.create(pause_at_start_time: true, desc_type: 'generated', audio_file_loc: "", desc_text: "rspec", voice_id: @voice.id, voice_speed: 1.3, desc_track_id: @track.id, start_time_sec: 15.4)
